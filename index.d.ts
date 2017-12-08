2244e2d429951ea45e7ee0f4b32faabd54c8e3ff
Kok J Sam <samkj.ks@gmail.com>
2017-12-08 11:38:49 -0500

    update typings

--- index.d.ts
+++ index.d.ts
@@ -3,40 +3,41 @@
 // K - Store state
 // I - Injected props to wrapped component
 declare module "unistore" {
-  import * as Preact from "preact";
+	import * as Preact from "preact";
 
-  export type Listener<K> = (state: K) => void;
+	export type Listener<K> = (state: K) => void;
+	export type Unsubscribe = () => void;
 
-  export interface Store<K> {
-    setState(update: object, overwrite: boolean): void;
-    subscribe(f: Listener<K>): void;
-    unsubscribe(f: Listener<K>): void;
-    getState(): K;
-  }
+	export interface Store<K> {
+		setState(update: object, overwrite?: boolean): void;
+		subscribe(f: Listener<K>): Unsubscribe;
+		unsubscribe(f: Listener<K>): void;
+		getState(): K;
+	}
 
-  export function createStore<K>(state: K): Store<K>;
+	export function createStore<K>(state?: K): Store<K>;
 
-  export type ActionFn<K> = (state: K) => object;
+	export type ActionFn<K> = (state: K) => object;
 
-  export interface ActionMap<K> {
-    [actionName: string]: ActionFn<K>;
-  }
+	export interface ActionMap<K> {
+		[actionName: string]: ActionFn<K>;
+	}
 
-  export type ActionCreator<K> = (store: Store<K>) => ActionMap<K>;
+	export type ActionCreator<K> = (store: Store<K>) => ActionMap<K>;
 
-  export type StateMapper<T, K, I> = (state: K, props?: T) => I;
+	export type StateMapper<T, K, I> = (state: K, props?: T) => I;
 
-  // TODO: Child should not be `any`.
-  export function connect<T, S, K, I>(
-    mapStateToProps: string | Array<string> | StateMapper<T, K, I>,
-    actions?: ActionCreator<K> | object
-  ): (Child: any) => Preact.ComponentConstructor<T, S>;
+	// TODO: Child should not be `any`.
+	export function connect<T, S, K, I>(
+		mapStateToProps: string | Array<string> | StateMapper<T, K, I>,
+		actions?: ActionCreator<K> | object
+	): (Child: any) => Preact.ComponentConstructor<T, S>;
 
-  export interface ProviderProps<T> {
-    store: Store<T>;
-  }
+	export interface ProviderProps<T> {
+		store: Store<T>;
+	}
 
-  export class Provider<T> extends Preact.Component<ProviderProps<T>, {}> {
-    render(props: ProviderProps<T>, {}): Preact.VNode;
-  }
+	export class Provider<T> extends Preact.Component<ProviderProps<T>, {}> {
+		render(props: ProviderProps<T>, { }): Preact.VNode;
+	}
 }
