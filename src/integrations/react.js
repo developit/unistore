88fcf4bf111217601ff13142ad2528ce5723840a
Jason Miller <developit@users.noreply.github.com>
2017-12-19 12:15:13 -0500

    Update react.js

--- src/integrations/react.js
+++ src/integrations/react.js
@@ -28,11 +28,11 @@ export function connect(mapStateToProps, actions) {
 				let mapped = mapStateToProps(store ? store.getState() : {}, this.props);
 				for (let i in mapped) if (mapped[i]!==state[i]) {
 					state = mapped;
-					return this.forceUpdate(null);
+					return this.forceUpdate();
 				}
 				for (let i in state) if (!(i in mapped)) {
 					state = mapped;
-					return this.forceUpdate(null);
+					return this.forceUpdate();
 				}
 			};
 			this.componentDidMount = () => {
