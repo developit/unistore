532087842c7c75525be1c4d56775374907630ee8
Naman Kheterpal <naman_kheterpal@yahoo.com>
2018-07-18 10:13:04 +0530

    Added types to work with stateful component
    Fix! these changes will solve the issue #103. Current definition file is giving type mismatch error with stateful components. With this both `connect` decorator and function are working fine.

--- react.d.ts
+++ react.d.ts
@@ -10,7 +10,7 @@ declare module "unistore/react" {
 	export function connect<T, S, K, I>(
 		mapStateToProps: string | Array<string> | StateMapper<T, K, I>,
 		actions?: ActionCreator<K> | object
-	): (Child: (props: T & I) => React.ReactNode) => React.Component<T, S>;
+	): (Child: ((props: T & I) => React.ReactNode) | (ComponentConstructor<T, S>)) => ComponentConstructor<T, S>;
 
 	export interface ProviderProps<T> {
 		store: Store<T>;
@@ -18,5 +18,9 @@ declare module "unistore/react" {
 
 	export class Provider<T> extends React.Component<ProviderProps<T>, {}> {
 		render(): React.ReactNode;
+	}
+	
+	interface ComponentConstructor<P = {}, S = {}> {
+		new(props: P, context?: any): React.Component<P, S>;
 	}
 }
