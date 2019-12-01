5e058170f6fbe18d323cd25520544304539639d1
Linus Miller <lohfu@lohfu.io>
2019-12-01 17:13:21 +0100

    Update types to match new action logic

--- index.d.ts
+++ index.d.ts
@@ -5,11 +5,15 @@
 
 export type Listener<K> = (state: K, action?: Action<K>) => void;
 export type Unsubscribe = () => void;
-export type Action<K> = (state: K, ...args: any[]) => void;
-export type BoundAction = (...args: any[]) => void;
+export type ActionFn<K> = (state: K, store: Store<K>) => Promise<Partial<K>> | Partial<K> | void;
+export interface ActionObject<K> {
+  type: string;
+  action: ActionFn<K>;
+}
+export type Action<K> = ActionObject<K> | ActionFn<K>
 
 export interface Store<K> {
-	action(action: Action<K>): BoundAction;
+	action(action: Action<K>): Promise<void> | void;
 	setState<U extends keyof K>(update: Pick<K, U>, overwrite?: boolean, action?: Action<K>): void;
 	subscribe(f: Listener<K>): Unsubscribe;
 	unsubscribe(f: Listener<K>): void;
@@ -18,12 +22,10 @@ export interface Store<K> {
 
 export default function createStore<K>(state?: K): Store<K>;
 
-export type ActionFn<K> = (state: K, ...args: any[]) => Promise<Partial<K>> | Partial<K> | void;
-
 export interface ActionMap<K> {
-	[actionName: string]: ActionFn<K>;
+	[actionName: string]: ActionCreator<K>;
 }
 
-export type ActionCreator<K> = (store: Store<K>) => ActionMap<K>;
+export type ActionCreator<K> = (...args: any[]) => Action<K>;
 
 export type StateMapper<T, K, I> = (state: K, props: T) => I;
