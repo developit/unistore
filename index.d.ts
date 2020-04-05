203dc79ef32bcf83ab7e649cadbad9c49da974c6
Linus Miller <linus.miller@bitmill.io>
2020-04-05 17:46:56 +0200

    Split Action type into separate AsyncAction and SyncAction

--- index.d.ts
+++ index.d.ts
@@ -5,12 +5,26 @@
 
 export type Listener<K> = (state: K, action?: Action<K>, update?: Partial<K>) => void;
 export type Unsubscribe = () => void;
-export type ActionFn<K> = (state: K, store: Store<K>) => Promise<Partial<K>> | Partial<K> | void;
-export interface ActionObject<K> {
+
+export type AsyncActionFn<K> = (getState: () => K, action: (action: Action<K>) => Promise<void> | void) => Promise<Partial<K> | void>;
+export type SyncActionFn<K> = (getState: () => K, action: (action: Action<K>) => Promise<void> | void) => Partial<K> | void;
+export type ActionFn<K> = AsyncActionFn<K> | SyncActionFn<K>;
+
+export type AsyncActionObject<K> = {
   type: string;
-  action: ActionFn<K>;
+  action: AsyncActionFn<K>;
 }
-export type Action<K> = ActionObject<K> | ActionFn<K>
+export type SyncActionObject<K> = {
+  type: string;
+  action: SyncActionFn<K>;
+}
+export type ActionObject<K> = AsyncActionObject<K> | SyncActionObject<K>;
+
+export type Action<K> = ActionObject<K> | ActionFn<K>;
+
+export type AsyncActionCreator<K> = (...args: any[]) => AsyncActionFn<K> | AsyncActionObject<K>;
+export type SyncActionCreator<K> = (...args: any[]) => SyncActionFn<K> | SyncActionObject<K>;
+export type ActionCreator<K> = AsyncActionCreator<K> | SyncActionCreator<K>;
 
 export interface Store<K> {
 	action(action: Action<K>): Promise<void> | void;
@@ -25,7 +39,5 @@ export default function createStore<K>(state?: K): Store<K>;
 export interface ActionMap<K> {
 	[actionName: string]: ActionCreator<K>;
 }
-
-export type ActionCreator<K> = (...args: any[]) => Action<K>;
 
 export type StateMapper<T, K, I> = (state: K, props: T) => I;
